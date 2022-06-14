c7ad8dd8cb10d126c85c11a8e94e61636efda119
Aleksei Bochkarev <aleksei.bochkarev@brandmaker.com>
2022-06-15 00:01:09 +0700

    add new tag systemProperties

--- src/test/java/guru/qa/properties/SystemPropertiesTests.java
+++ src/test/java/guru/qa/properties/SystemPropertiesTests.java
@@ -3,6 +3,7 @@ package guru.qa.properties;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+@Tag("systemProperties")
 public class SystemPropertiesTests {
     @Test
     void someTest () {
